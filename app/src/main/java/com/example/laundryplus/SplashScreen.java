5c51cefa8c8a19b3b5dd32653d884139d39789ad
rendra113 <rendramuhammad113@gmail.com>
2020-03-27 22:57:06 +0700

    27 Maret 2020

---
+++ app/src/main/java/com/example/laundryplus/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.laundryplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private int waktu = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashScreen.this , MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },waktu);
+    }
+}
